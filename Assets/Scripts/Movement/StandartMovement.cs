3e4805d0ddc81ec428096194c9896be9e0b9c084
Cmanchik <aksenova1648@gmail.com>
2025-11-10 01:50:33 +0400

    Поворот объекта в сторону движения

--- Assets/Scripts/Movement/StandartMovement.cs
+++ Assets/Scripts/Movement/StandartMovement.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts.Movement
 {
     [RequireComponent(typeof(Rigidbody2D))]
     public class StandartMovement : AbstractMovement
     {
+        [SerializeField]
+        private Transform m_rotatableObject;
+
         private Rigidbody2D m_rb;
 
         private void Start()
@@ -16,6 +18,13 @@ namespace Assets.Scripts.Movement
         protected override void Move()
         {
             m_rb.velocity = Direction * m_speed;
+            Rotate();
+        }
+
+        private void Rotate()
+        {
+            if (Direction.x > 0) m_rotatableObject.rotation = Quaternion.Euler(0, 0, 0);
+            else if (Direction.x < 0) m_rotatableObject.rotation = Quaternion.Euler(0, 180, 0);
         }
     }
 }
